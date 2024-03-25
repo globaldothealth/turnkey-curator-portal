083ca7505f2b1847d99dc64f71dab5728227b35b
jim-sheldon <5619539+jim-sheldon@users.noreply.github.com>
2024-03-25 13:11:24 -0400

    Updating data export job and matching new schema

--- data-serving/scripts/export-data/full_export.sh
+++ data-serving/scripts/export-data/full_export.sh
@@ -1,30 +1,20 @@
 #!/bin/bash
 # Full data export
-# Depends: aws-cli
 
 set -xeuo pipefail
-source ./common.sh
+
+require_env() {
+  if [ -z "$1" ]; then
+      echo "$2"
+      exit 1
+  fi
+}
+
 require_env "${FULL_EXPORT_BUCKET:-}" "Specify FULL_EXPORT_BUCKET"
 require_env "${CONN:-}" "Specify MongoDB connection string in CONN"
 
+mongoexport --uri="$CONN" --collection=day0cases --fieldFile=fields.txt --type=csv --out full.csv
 
-SCRATCH="$(mktemp -d)"
-BUCKETS="${SCRATCH}/buckets.json"
-ALL_DATA="${SCRATCH}/all_data.csv"
-trap 'rm -rf "$SCRATCH"' EXIT  # Cleanup before exit
+gzip full.csv
 
-FORMAT="${FORMAT:-csv,tsv,json}"
-QUERY="{\"curators.verifiedBy\": { \"\$exists\": \"true\"}}"
-
-mongoexport --uri="$CONN" --collection=ageBuckets --type=json --jsonArray -o "${BUCKETS}"
-mongoexport --query="$QUERY" --uri="$CONN" --collection=day0cases \
-    --fieldFile=fields.txt --type=csv -o "${ALL_DATA}"
-python3 transform.py -f "$FORMAT" -b "${BUCKETS}" -i "${ALL_DATA}" "full"
-
-# ignore shellcheck warning on word splitting, as it's actually needed here
-# shellcheck disable=SC2086
-for fmt in ${FORMAT//,/ }
-do
-   test -f "full.${fmt}.gz" && aws s3 cp "full.${fmt}.gz" "s3://${FULL_EXPORT_BUCKET}/${fmt}/"
-   rm "full.${fmt}.gz"
-done
+aws s3 cp full.csv.gz "s3://${FULL_EXPORT_BUCKET}/csv/"
