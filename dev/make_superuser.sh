25a86d5efdc0cabc7c83cfb5162f0ae9562d97fb
Stanisław Zakrzewski <41862803+stanislaw-zakrzewski@users.noreply.github.com>
2024-10-02 12:57:27 +0200

    [152] mongodb 5.0 end of life on october 31 2024 (#153)
    * Update mongo version in main dockerfile
    
    * Update script for adding admin role to user
    
    * Update rest of the mongo instances in databases

--- dev/make_superuser.sh
+++ dev/make_superuser.sh
@@ -10,6 +10,6 @@ cd `dirname "$0"`
 # Pass database name as the first parameter
 DB="${GDH_DATABASE:-$1}"
 # Pass email of user to grant an admin role as second parameter
-docker-compose -f docker-compose.yml -f docker-compose.dev.yml exec mongo mongo "${DB}" --eval "var email='$2'; var roles=['admin', 'curator'];" /verification/scripts/roles.js
+docker-compose -f docker-compose.yml -f docker-compose.dev.yml exec mongo mongosh "${DB}" --eval "var email='$2'; var roles=['admin', 'curator'];" /verification/scripts/roles.js
 # Restore directory.
 popd
